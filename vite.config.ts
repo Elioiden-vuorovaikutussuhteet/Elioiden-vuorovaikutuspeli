5a3cff72b56afe30c89dd5c90a2b7e7eb035b83c
eeroHY <eero.lumijarvi@helsinki.fi>
2026-09-09 14:40:25 +0300

    Modified vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+//vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:4173",
+        changeOrigin: true, 
+      }
+    }
+  }
 })
