1e9ef7fcc264e889f27f0688643791bfc8b5fdc8
TommiWindows <tommi.kohonen21@gmail.com>
2026-09-14 14:13:50 +0300

    Coverage edit to try to fix codecov

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 //vite.dev/config/
 export default defineConfig({
@@ -17,7 +17,7 @@ export default defineConfig({
 
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'lcov'],
     },
   },
 })
