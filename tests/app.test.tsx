40756b77481231ecfcfe273dbdc893306a9ba829
TommiAL <kohonen.tommi21@gmail.com>
2026-09-16 12:59:43 +0300

    Removed obsolete test that does not work

--- tests/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { describe, it } from 'vitest'
-import App from '../src/App'
-
-describe("App test", () => {
-    it('renders successfully', () => {
-        render(<App />)     
-    })
-})
