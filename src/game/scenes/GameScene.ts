a2b1078d3cf28ae3e104e971c33b84449039837b
onnanna <hakkanna@cubbli24-gold.cs.helsinki.fi>
2026-09-16 14:52:09 +0300

    Fix bullhorn acacia

--- src/game/scenes/GameScene.ts
+++ src/game/scenes/GameScene.ts
@@ -6,14 +6,21 @@ export default class GameScene extends Phaser.Scene {
     super("GameScene");
   }
 
-  preload() {}
+  preload() {
+    this.load.setPath("assets");
+    this.load.image('bullhornacacia', 'bullhornacacia.png');
+  }
 
   create() {
+    const centerX = this.cameras.main.centerX;
+    const centerY = this.cameras.main.centerY;
+
     this.scene.launch("MenuScene");
     this.time.delayedCall(5000, () => {
       console.log("5 seconds passed!");
     });
-    this.bullhornacacia = this.add.image(512, 384, 'bullhornacacia');
+    this.bullhornacacia = this.add.image(centerX, centerY, 'bullhornacacia');
+    this.bullhornacacia.setScale(1.5);
   }
 
   update() {}
